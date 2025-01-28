63aff788e831cdcee57122e0174e3f31b15c65fb
Everly <66821749+everlyy@users.noreply.github.com>
2025-01-28 16:08:27 +0100

    Only create example config if it doesn't exist

--- venv.sh
+++ venv.sh
@@ -3,17 +3,22 @@
 VENV_DIR=".venv"
 PYTHON="/usr/bin/env python3"
 REQUIREMENTS="requirements.txt"
+CONFIG_FILE="config.py"
 
 function activate {
     source $VENV_DIR/bin/activate
 }
 
+if [[ ! -f "$CONFIG_FILE" ]] then
+    # Create example config file
+    echo "DISCORD_TOKEN = \"token here\"" >> $CONFIG_FILE
+fi
+
 if [[ ! -d "$VENV_DIR" ]] then
+    # Create virtual environment and install required packages
     $PYTHON -m venv "$VENV_DIR"
     activate
     pip install -r "$REQUIREMENTS"
-
-    echo "DISCORD_TOKEN = \"token here\"" > config.py
 else
     activate
 fi
